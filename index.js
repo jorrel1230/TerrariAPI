5e20467add5908ded13d0cc8fc28140d9d06a2d4
jorrel1230 <jr8867@princeton.edu>
2024-05-27 20:24:29 -0400

    Testing Bosses API Endpoint

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 var database = require('./database.json');
 
 app.use(express.json());
 
-app.get('/id/:id', (req, res) => {
+app.get('/item/:id', (req, res) => {
 
     const { id } = req.params;
     res.status(200).send({
         id: id,
         payload: database[id]
+    })
+
+});
+
+app.get('/bosses/:name', (req, res) => {
+
+    const { name } = req.params;
+    res.status(200).send({
+        payload: bosses[name]
     })
 
 });
